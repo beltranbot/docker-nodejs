10c10a99a778b2846986faf87f43f49ad22acdc8
beltranbot <beltranbot@gmail.com>
2021-08-27 20:04:01 -0500

    added docker-compose support for multiple environments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-    res.send("<h2> Hi There xxx</h2>")
+    res.send("<h2> Hi There </h2>")
 })
 
 app.listen(port, () => console.log(`listening to port ${port}`))
