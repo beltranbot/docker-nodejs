24fa9db9d6b93bc5dcebde3f63400d1ad5ccb267
beltranbot <beltranbot@gmail.com>
2021-08-27 22:44:13 -0500

    added mongo service and connect function

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config")
+
 const app = express()
-const port = process.env.PORT || 3000
+
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+const connectWithRetry = () => {
+    mongoose.connect(mongoUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("sucesfully connected to DB"))
+    .catch((e) => {
+        console.log(e)
+        setTimeout(connectWithRetry, 5000)
+    })
+}
+
+connectWithRetry()
 
 app.get("/", (req, res) => {
     res.send("<h2> Hi There </h2>")
 })
+
+const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`listening to port ${port}`))
