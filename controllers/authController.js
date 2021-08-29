f3e00c7729ba33d5e087998bc78dbad193868543
beltranbot <beltranbot@gmail.com>
2021-08-29 18:25:51 -0500

    added user signup and login

---
+++ controllers/authController.js
@@ -0,0 +1,50 @@
+const User = require("../models/userModel")
+const bcrypt = require("bcryptjs")
+
+exports.signUp = async (req, res) => {
+    const { username, password } = req.body
+    try {
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const newUser = await User.create({
+            username,
+            password: hashedPassword
+        })
+        res.status(201).json({
+            status: "success",
+            data: {
+                user: newUser
+            }
+        })
+    } catch (e) {
+        res.status(400).json({
+            status: "fail"
+        })
+    }
+}
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body
+    try {
+        const user = await User.findOne({ username })
+        if (!user) {
+            return res.status(404).json({
+                status: "fail",
+                message: "incorrect username or password"
+            })
+        }
+        const isCorrect = await bcrypt.compare(password, user.password)
+        if (!isCorrect) {
+            res.status(401).json({
+                status: "incorrect username or password",
+            })
+            
+        }
+        return res.status(200).json({
+            status: "success",
+        })
+    } catch (e) {
+        res.status(500).json({
+            status: "fail"
+        })
+    }
+}
